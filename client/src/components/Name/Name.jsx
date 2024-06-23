12329714c8e6f6166d780344c864fb7bcc554a36
irene <panisirene@gmail.com>
2024-06-22 21:56:39 -0700

    create Name component with passed handleRemoveUser prop

---
+++ client/src/components/Name/Name.jsx
@@ -0,0 +1,8 @@
+export const Name = (username, onRemove) => {
+  return (
+    <div>
+      <p>{username}</p>
+      <button type="button" onClick={() => onRemove(username)}>×</button>
+    </div>
+  )
+}
