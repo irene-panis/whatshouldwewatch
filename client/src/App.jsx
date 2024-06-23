c297c0b8c306b49c31416ffc26b9c542e14cc8e6
irene <panisirene@gmail.com>
2024-06-22 22:03:29 -0700

    add SearchBar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.scss'
+import { SearchBar } from './components/SearchBar/SearchBar'
 
 function App() {
   return (
     <>
       <h1>what should we watch?</h1>
+      <SearchBar/>
     </>
   )
 }
