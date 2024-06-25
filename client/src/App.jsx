a7bf1ec52e624d6b710ea885accb3c851ad3e610
irene <panisirene@gmail.com>
2024-06-24 21:28:16 -0700

    add main element and footer to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { SearchBar } from './components/SearchBar/SearchBar'
 import { useState } from 'react';
 import { Results } from './components/Results/Results';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   const [usernames, setUsernames] = useState([]);
@@ -11,10 +12,13 @@ function App() {
   }
   return (
     <>
-      <h1>what should we watch?</h1>
-      <h2>Enter 2+ Letterboxd usernames below to find the overlap between your watchlists.</h2>
-      <SearchBar usernames={usernames} setUsernames={setUsernames} onSubmit={handleSubmitUsers}/>
-      {results && <Results results={results}/>}
+      <main>
+        <h1>what should we watch?</h1>
+        <h2>Enter 2+ Letterboxd usernames below to find the overlap between your watchlists.</h2>
+        <SearchBar usernames={usernames} setUsernames={setUsernames} onSubmit={handleSubmitUsers}/>
+        {results && <Results results={results}/>}
+      </main>
+      <Footer/>
     </>
   )
 }
