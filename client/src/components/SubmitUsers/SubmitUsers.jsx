33da0b8574e0191e911e053ec341af8817ead334
irene <panisirene@gmail.com>
2024-06-25 20:31:35 -0700

    center submit button

--- client/src/components/SubmitUsers/SubmitUsers.jsx
+++ client/src/components/SubmitUsers/SubmitUsers.jsx
@@ -4,6 +4,7 @@ export const SubmitUsers = ({ onSubmit }) => {
   return (
     <button 
       type="button"
+      className="submit-btn"
       onClick={onSubmit}
     >
       What Should We Watch?
