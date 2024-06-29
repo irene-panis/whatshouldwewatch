6f2631575cacd028d37477542679d190a5db4cc0
irene <panisirene@gmail.com>
2024-06-29 16:01:55 -0700

    style results

--- client/src/components/Results/Results.jsx
+++ client/src/components/Results/Results.jsx
@@ -2,16 +2,18 @@ import './Results.css'
 
 export const Results = ({ results }) => {
   return (
-    <div className="results-container">
-      {
-        results ? (
-          results.map((movie, index) => (
-            <p key={index}>{movie}</p>
-          ))
-        ) : (
-          <p>No results found!</p>
-        )
-      }
-    </div>
+    <>
+      <div className="results-container">
+        {
+          results ? (
+            results.map((movie, index) => (
+              <p key={index} className="result">{movie}</p>
+            ))
+          ) : (
+            <p>0 overlaps found. Please try again.</p>
+          )
+        }
+      </div>
+    </>
   )
 }
