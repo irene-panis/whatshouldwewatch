5e2996ee8c115bd4fcf26839630289b4fd6432b8
irene <panisirene@gmail.com>
2024-06-29 15:43:08 -0700

    return json data at / endpoint

--- server/server.py
+++ server/server.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+def my_function():
+  result = {"message": "This is the result of my function."}
+  return result
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  result = my_function()
+  return jsonify(result)
