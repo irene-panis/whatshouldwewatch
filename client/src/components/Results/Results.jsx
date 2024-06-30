ba1a37c30d044f9672c53921988213ae149d3f69
irene <panisirene@gmail.com>
2024-06-30 16:12:42 -0700

    map over results and render overlap groups

--- client/src/components/Results/Results.jsx
+++ client/src/components/Results/Results.jsx
@@ -1,19 +1,23 @@
 import './Results.css'
 
 export const Results = ({ results }) => {
+  const groups = Object.keys(results.overlap_groups).sort((a, b) => parseInt(b) - parseInt(a));
   return (
     <>
       <div className="results-container">
-        {
-          results ? (
-            results.map((movie, index) => (
-              <a href={movie.link} target="_blank" key={index} className="result">{movie.title}</a>
-            ))
-          ) : (
-            <p>0 overlaps found. Please try again.</p>
-          )
-        }
+        {groups.map((groupNum, index) => (
+          <div key={index} className="group-container">
+            <p>Overlaps in {groupNum} watchlists</p>
+            <div className="group-results">
+              {
+                results.overlap_groups[groupNum].map((movie, index) => (
+                  <a key={index} className="result" href={movie.link}>{movie.title}</a>
+                ))
+              }
+            </div>
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
