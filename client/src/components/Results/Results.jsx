4fdf5dd510997a454fe21490659023a205715267
irene <panisirene@gmail.com>
2024-06-29 19:45:35 -0700

    make movies anchors and style for hover

--- client/src/components/Results/Results.jsx
+++ client/src/components/Results/Results.jsx
@@ -7,7 +7,7 @@ export const Results = ({ results }) => {
         {
           results ? (
             results.map((movie, index) => (
-              <p key={index} className="result">{movie.title}</p>
+              <a href={movie.link} target="_blank" key={index} className="result">{movie.title}</a>
             ))
           ) : (
             <p>0 overlaps found. Please try again.</p>
