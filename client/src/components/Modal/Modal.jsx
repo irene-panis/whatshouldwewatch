f75efacfd8a944a64f0785435ac9d1dd36cf2c9f
irene <panisirene@gmail.com>
2024-07-04 21:00:59 -0700

    create modal

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,5 +1,15 @@
-export const Modal = () => {
+import './Modal.css';
+
+export const Modal = ({ showModal, setShowModal, children }) => {
+  if (!showModal) return null;
   return (
-    <></>
-  )
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-button" onClick={() => setShowModal(false)}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
 }
