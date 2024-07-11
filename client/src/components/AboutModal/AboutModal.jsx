ea3b3552b4bd2d13348ae3c6d7773565969b05af
irene <panisirene@gmail.com>
2024-07-10 20:30:43 -0700

    tweak user instructions

--- client/src/components/AboutModal/AboutModal.jsx
+++ client/src/components/AboutModal/AboutModal.jsx
@@ -2,8 +2,8 @@ export const AboutModal = () => {
   return (
     <>
       <p><span>what should we watch?</span> scrapes the letterboxd watchlists of the users provided and returns the movies present in at least 50% and up to 100% of the lists.</p>
-      <p>there is a minimum of 2 users entered required for this app and a maximum of 10.</p>
-      <p>to avoid repeated scraping, <span>user watchlist data is cached for one hour after first retrieval.</span> if you run your username through the app then make a change, it will not be reflected until you delete your username from our cache. see "cache" tab to do this.</p>
+      <p>a minimum of 2 users must be entered to scrape the lists, with a maximum of 10 users.</p>
+      <p>to avoid repeated scraping, <span>user watchlist data is cached for one hour after first retrieval.</span> this makes the app faster if you're sending multiple requests for the same watchlist. if a username is ran through the app and a change is made to the watchlist afterwards, it will not be reflected until the username is manually deleted from our cache or it expires after 1 hour. see "cache" tab to clear the cache.</p>
     </>
   )
 }
