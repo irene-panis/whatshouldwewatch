e7aec20956417ef5c3e0682d728b61d2072096f6
irene <panisirene@gmail.com>
2024-07-14 19:44:16 -0700

    create input and button for deleting user from cache

--- client/src/components/DeleteCache/DeleteCache.jsx
+++ client/src/components/DeleteCache/DeleteCache.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
 export const DeleteCache = () => {
   const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  
   return (
-    <></>
-  )
+    <div className="delete-bar">
+      <div>
+        <label htmlFor="username" className="sr-only">
+          Letterboxd Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Letterboxd username (e.g., irenesucks)"
+          required
+        />
+      </div>
+      <button type="button" onClick={() => handleDeleteUser(input)}>
+        Delete
+      </button>
+    </div>
+  );
 }
