7a90bca77bb58687dc8475a2685d76690fb02c30
irene <panisirene@gmail.com>
2024-07-18 22:52:58 -0700

    add colors to about modal

--- client/src/components/AboutModal/AboutModal.jsx
+++ client/src/components/AboutModal/AboutModal.jsx
@@ -7,7 +7,7 @@ export const AboutModal = () => {
       <p><b>what should we watch?</b> scrapes the letterboxd watchlists of the users provided and returns the movies present in at least 50% and up to 100% of the lists.</p>
       <p>a minimum of 2 users must be entered to scrape the lists, with a maximum of 10 users.</p>
       <p>to avoid repeated scraping, <i>user watchlist data is cached for one hour after first retrieval.</i> this makes the app faster if you're sending multiple requests for the same watchlist. if a username is ran through the app and a change is made to the watchlist afterwards, it will not be reflected until the username is manually deleted from our cache or it expires after 1 hour.</p>
-      <p>to force retrieve a user's latest data, delete them from the cache below:</p>
+      <span>to force retrieve a user's latest data, delete them from the cache below:</span>
       <DeleteCache/>
     </div>
   )
