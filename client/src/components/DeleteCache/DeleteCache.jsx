1b03f036f77776bfd9e23ed19dcd7af1a9e61354
irene <panisirene@gmail.com>
2024-07-20 16:57:27 -0700

    fix deletecache component

--- client/src/components/DeleteCache/DeleteCache.jsx
+++ client/src/components/DeleteCache/DeleteCache.jsx
@@ -3,36 +3,43 @@ import { useState } from "react";
 
 export const DeleteCache = () => {
   const [input, setInput] = useState('');
+  const [showConfirm, setShowConfirm] = useState('');
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
+    setShowConfirm(false);
   }
 
   const handleDeleteUser = async () => {
-    await fetch(`http://localhost:5000/${input}`, {
+    const response = await fetch(`http://localhost:5000/delete/${input}`, {
       method: 'DELETE'
     });
+    const data = await response.json();
+    setShowConfirm(data.message);
     setInput('');
   }
   return (
-    <div className="delete-bar">
-      <div>
-        <label htmlFor="username" className="sr-only">
-          Letterboxd Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={input}
-          onChange={handleInputChange}
-          placeholder="Letterboxd username (e.g., irenesucks)"
-          required
-        />
+    <div>
+      <div className="delete-bar">
+        <div>
+          <label htmlFor="username" className="sr-only">
+            Letterboxd Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Letterboxd username (e.g., irenesucks)"
+            required
+          />
+        </div>
+        <button type="button" onClick={() => handleDeleteUser(input)}>
+          Delete
+        </button>
       </div>
-      <button type="button" onClick={() => handleDeleteUser(input)}>
-        Delete
-      </button>
+      {showConfirm && <p>{showConfirm}</p>}
     </div>
   );
 }
